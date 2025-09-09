6d58e9088843d926c844e84066e1ffb05c64e55c
biah3308 <bianca.lais@escolar.ifrn.edu.br>
2025-09-09 10:17:19 -0300

    projeto2

---
+++ projeto2/Assets/player/dano.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class dano : MonoBehaviour
+{
+    public class Chave : MonoBehaviour
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
